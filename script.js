2afe9241d589bfaf43dc917aedb8b370b47f4818
kingvig <vignydjoumessi@gmail.com>
2023-03-08 14:28:37 +0100

    assynchronous javascript using promises to fetch data from a web api

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict';
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data) {
+  const html = `
+    <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${data.population}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+    </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = '1';
+};
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then((response) =>response.json())
+    .then( (data)=>{
+        renderCountry(data[0]);
+        const neighbour=data[0].borders[0];
+        if (!neighbour) {
+            return;
+        }
+        return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    }).then(response => response.json())
+    .then(data=>renderCountry(data));
+};
+
+getCountryData('usa');
